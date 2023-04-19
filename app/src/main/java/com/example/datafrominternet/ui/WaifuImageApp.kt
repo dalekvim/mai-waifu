3d47a3c425aca3a676674618898ed514faf55aab
Vimal Vinod <dalekvim@gmail.com>
2023-04-19 23:45:39 +0100

    Implemented filtering by tags.

--- app/src/main/java/com/example/datafrominternet/ui/WaifuImageApp.kt
+++ app/src/main/java/com/example/datafrominternet/ui/WaifuImageApp.kt
@@ -7,20 +7,48 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datafrominternet.ui.fragments.WaifuSheetContentState
 import com.example.datafrominternet.ui.fragments.WaifuTopAppBar
 import com.example.datafrominternet.ui.screens.HomeScreen
+import com.example.datafrominternet.ui.screens.Tags
 import com.example.datafrominternet.ui.screens.WaifuViewModel
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun WaifuImageApp() {
     val waifuViewModel: WaifuViewModel = viewModel(factory = WaifuViewModel.Factory)
 
+    var includedTags by rememberSaveable {
+        mutableStateOf(emptyList<Tags>())
+    }
+
+    val toggleIncludedTags = fun(tag: Tags) {
+        includedTags = if (tag in includedTags) {
+            includedTags.filter { it != tag }
+        } else {
+            includedTags + tag
+        }
+    }
+
+    val clearTags = fun() {
+        includedTags = emptyList()
+    }
+
     BottomSheetScaffold(
-        topBar = { WaifuTopAppBar(waifuViewModel = waifuViewModel) },
+        topBar = {
+            WaifuTopAppBar(
+                waifuViewModel = waifuViewModel,
+                includedTags = includedTags,
+                clearTags = clearTags
+            )
+        },
         sheetContent = {
             Surface(
                 color = MaterialTheme.colorScheme.background,
@@ -28,6 +56,8 @@ fun WaifuImageApp() {
             ) {
                 WaifuSheetContentState(
                     waifuViewModel = waifuViewModel,
+                    includedTags = includedTags,
+                    toggleIncludedTags = toggleIncludedTags,
                     modifier = Modifier.fillMaxSize()
                 )
             }
@@ -41,6 +71,7 @@ fun WaifuImageApp() {
         ) {
             HomeScreen(
                 waifuViewModel = waifuViewModel,
+                includedTags = includedTags,
                 modifier = Modifier
                     .fillMaxSize()
             )
--- app/src/main/java/com/example/datafrominternet/ui/fragments/WaifuSheetContent.kt
+++ app/src/main/java/com/example/datafrominternet/ui/fragments/WaifuSheetContent.kt
@@ -3,7 +3,8 @@ package com.example.datafrominternet.ui.fragments
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,15 +12,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.datafrominternet.R
 import com.example.datafrominternet.model.Waifu
+import com.example.datafrominternet.ui.screens.Tags
 import com.example.datafrominternet.ui.screens.WaifuUiState
 import com.example.datafrominternet.ui.screens.WaifuViewModel
+import com.example.datafrominternet.ui.screens.stringToTag
 
 @Composable
 fun WaifuSheetContentState(
     waifuViewModel: WaifuViewModel,
+    includedTags: List<Tags>,
+    toggleIncludedTags: (tag: Tags) -> Unit,
     modifier: Modifier
 ) {
-
     when (val waifuUiState = waifuViewModel.waifuUiState) {
 
         is WaifuUiState.Loading -> {}
@@ -29,6 +33,8 @@ fun WaifuSheetContentState(
 
             WaifuSheetContent(
                 image = image,
+                includedTags = includedTags,
+                toggleIncludedTags = toggleIncludedTags,
                 modifier = modifier
             )
         }
@@ -39,9 +45,12 @@ fun WaifuSheetContentState(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WaifuSheetContent(
     image: Waifu,
+    includedTags: List<Tags>,
+    toggleIncludedTags: (tag: Tags) -> Unit,
     modifier: Modifier
 ) {
     Row(
@@ -51,14 +60,13 @@ fun WaifuSheetContent(
     ) {
         if (image.tags != null) {
             for (tag in image.tags) {
-                Button(
-                    modifier = Modifier.padding(horizontal = 4.dp),
-                    onClick = { /*TODO*/ }
-                ) {
-                    Text(text = tag.name)
-                }
+                FilterChip(
+                    label = { Text(text = tag.name) },
+                    selected = (stringToTag(tag.name) in includedTags),
+                    onClick = { toggleIncludedTags(stringToTag(tag.name)) },
+                    modifier = Modifier.padding(horizontal = 4.dp)
+                )
             }
         }
     }
-
 }
